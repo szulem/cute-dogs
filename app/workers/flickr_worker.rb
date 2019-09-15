effc4e90bc1404a7b3e71fee30d9b95cd634ebd2
szulem <mirek@szul.eu>
2019-09-15 21:02:56 +0200

    Changed flickr_worker

--- app/workers/flickr_worker.rb
+++ app/workers/flickr_worker.rb
@@ -3,15 +3,15 @@ class FlickrWorker
   include Sidekiq::Worker
 
   def perform
-    flickr = FlickRaw::Flickr.new
     counter = 0
+    flickr = FlickRaw::Flickr.new
 
     while counter < 100 do
       album = flickr.photos.search(tags: "dog")
       album.reject { |dog| Dog.all.pluck(:image).include?(FlickRaw.url_q(dog))}.each do |obj|
         photo = FlickRaw.url_q(obj)
         new_dog = Dog.new(image: photo, cuteness_score: 0)
-        if new_dog.save!
+        if new_dog.save
           counter += 1
         end
       end
