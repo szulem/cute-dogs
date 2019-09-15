711ab841242d7b3287e380244e270ce22ec2601d
szulem <mirek@szul.eu>
2019-09-15 19:32:03 +0200

    Added Sidekiq routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  require 'sidekiq/cron/web'
+  mount Sidekiq::Web => '/sidekiq'
   get 'dogs/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
