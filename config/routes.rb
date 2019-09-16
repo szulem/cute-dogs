d18ff36a7e9e0b1c421a51c2206f690c1dab7ed3
szulem <mirek@szul.eu>
2019-09-16 02:45:22 +0200

    Added voting routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   require 'sidekiq/cron/web'
   mount Sidekiq::Web => '/sidekiq'
-  get 'dogs/index'
+  get 'dogs/secured'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   root to: "home#index"
+
+  resources :dogs do
+    member do
+      put "like" => "dogs#upvote"
+    end
+  end
+
 end
