35ef2fa362639aaf4ae43b72d1794513db474f56
szulem <mirek@szul.eu>
2019-09-18 18:26:50 +0200

    Changed redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV['REDIS_URL'] }
   schedule_file = "config/sidekiq.yml"
   if File.exists?(schedule_file)
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
@@ -7,5 +7,4 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
-end
+  config.redis = { url: ENV['REDIS_URL'] }
